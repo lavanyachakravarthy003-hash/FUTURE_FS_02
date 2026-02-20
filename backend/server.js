83d8b6b33e6188c86576c2781572880cfe277f30
lavanyachakravarthy003-hash <lavanyachakravarthy003@gmail.com>
2026-02-20 22:20:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.get("/", (req, res) => {
   res.send("CRM Backend Running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
